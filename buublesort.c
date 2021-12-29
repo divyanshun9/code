62b31d1ac6073b36813865d93c13b0870b38e2ca
Divyanhu negi <90817183+divyanshun9@users.noreply.github.com>
2021-12-29 21:47:02 +0530

    Create buublesort.c

---
+++ buublesort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void bubble(int *arr, int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+       for(int j= 0;j<n-i-1;j++)
+       {
+           if(arr[j]>arr[j+1])
+           {
+               int temp = arr[j];
+               arr[j]= arr[j+1];
+               arr[j+1]= temp;
+           }
+       }
+    }
+    
+    for(int i=0;i<n;i++)
+     printf("%d ",arr[i]);
+}
+
+
+int main()
+{
+  int n;
+  printf("enter the number of elements\n");
+  scanf("%d",&n);
+  int arr[n];
+  printf("enter the elements of array \n");
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",&arr[i]);  
+  }
+  
+  bubble(arr,n);
+}
